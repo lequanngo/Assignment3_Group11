41429008c1f44e7f1b3181ff99cd39dd4613ba1f
AnitMariaJoseph <31916421+AnitMariaJoseph@users.noreply.github.com>
2019-04-14 14:11:04 -0700

    Update HelloWorldApplication.java

--- HelloWorld/src/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/HelloWorld/HelloWorldApplication.java
@@ -37,6 +37,7 @@ public class HelloWorldApplication extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		System.out.println("This isw an application using swing");
 	}
 
 }
