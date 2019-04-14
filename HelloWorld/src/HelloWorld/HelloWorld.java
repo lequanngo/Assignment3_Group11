647f130ee01a8c01c5f0f824deb78dbaa6373d90
Ngo <ngol3@student.douglascollege.ca>
2019-04-14 13:09:05 -0700

    Initial commit

--- HelloWorld/src/HelloWorld/HelloWorld.java
+++ HelloWorld/src/HelloWorld/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World");
-
+		//add Le Quan Ngo to HelloWorld
+		System.out.println("Add Le Quan Ngo");
 	}
 
 }
